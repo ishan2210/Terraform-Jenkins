15081ef226a38675fa2a1e81b2b313357af7725a
Ishan Kansara <84899823+ishan2210@users.noreply.github.com>
2025-01-04 00:35:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,25 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "test1" {
   bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "test2" {
+  bucket = "my-tf-test-bucket2"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+resource "aws_s3_bucket" "test3" {
+  bucket = "my-tf-test-bucket3"
 
   tags = {
     Name        = "My bucket"

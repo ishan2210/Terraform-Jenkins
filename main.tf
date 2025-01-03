8eb8ba6214be8a01abc522190aef7e69630f84b1
Ishan Kansara <84899823+ishan2210@users.noreply.github.com>
2025-01-04 00:31:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
@@ -16,22 +9,6 @@ resource "aws_s3_bucket" "example" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-  }
-}
-
-resource "aws_instance" "test-web" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
-
-resource "aws_instance" "web-server2" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
   }
 }
 

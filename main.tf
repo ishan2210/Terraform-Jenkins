fab5a3e1acac88d25c571958ec6658048178eafe
Ishan Kansara <84899823+ishan2210@users.noreply.github.com>
2025-01-03 17:18:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,11 @@ resource "aws_instance" "test-web" {
   }
 }
 
+resource "aws_instance" "web-server2" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
+
